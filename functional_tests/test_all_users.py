31658b893b6a8d61b42811e0a0e696e8db2aab0f
Kewal Panchputre <kpanchputre@Mike-MBP.local>
2017-05-19 16:16:05 -0500

    Internationalization and localization

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -3,14 +3,18 @@ from selenium import webdriver
 import unittest,os
 from django.core.urlresolvers import reverse
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.utils.translation import activate
+from datetime import date
+from django.utils import formats
 
 class HomeNewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
-        chromedriver = "/Users/kpanchputre/Downloads/chromedriver"
+        chromedriver = "/Users/kpanchputre/Applications/chromedriver"
         os.environ["webdriver.chrome.driver"] = chromedriver
         self.browser = webdriver.Chrome(chromedriver)
         self.browser.implicitly_wait(3)
+        activate('en')
 
     def tearDown(self):
         self.browser.quit()
@@ -27,3 +31,35 @@ class HomeNewVisitorTest(StaticLiveServerTestCase):
         h1 = self.browser.find_element_by_tag_name("h1")
         self.assertEqual(h1.value_of_css_property("color"),
                          "rgba(200, 50, 255, 1)")
+    def test_home_files(self):
+        self.browser.get(self.live_server_url + "/robots.txt")
+        self.assertNotIn("Not Found", self.browser.title)
+        self.browser.get(self.live_server_url + "/humans.txt")
+        self.assertNotIn("Not Found", self.browser.title)
+
+    def test_internationalization(self):
+        for lang, h1_text in [('en', 'Welcome to Mysite!'),
+                                    ('ca', 'Benvingut a Mysite!')]:
+            activate(lang)
+            self.browser.get(self.get_full_url("home"))
+            h1 = self.browser.find_element_by_tag_name("h1")
+            self.assertEqual(h1.text, h1_text)
+
+    def test_localization(self):
+        today = date.today()
+        for lang in ['en', 'ca']:
+            activate(lang)
+            self.browser.get(self.get_full_url("home"))
+            local_date = self.browser.find_element_by_id("local-date")
+            non_local_date = self.browser.find_element_by_id("non-local-date")
+            self.assertEqual(formats.date_format(today, use_l10n=True),
+                                  local_date.text)
+            self.assertEqual(today.strftime('%Y-%m-%d'), non_local_date.text)
+
+    def test_time_zone(self):
+        self.browser.get(self.get_full_url("home"))
+        tz = self.browser.find_element_by_id("time-tz").text
+        utc = self.browser.find_element_by_id("time-utc").text
+        ny = self.browser.find_element_by_id("time-ny").text
+        self.assertNotEqual(tz, utc)
+        self.assertNotIn(ny, [tz, utc])
--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -68,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
             ],
         },
     },
@@ -113,9 +115,19 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
+from django.utils.translation import ugettext_lazy as _
+LANGUAGES = (
+    ('en', _('English')),
+    ('ca', _('Catalan')),
+)
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
+
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
